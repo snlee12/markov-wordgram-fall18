a8d69f3e1cd87183a5359a07ba13120a6fa9a8e7
Siyun Lee <snlee12@gmail.com>
2018-09-24 19:57:38 -0400

    All green test cases

--- src/WordGram.java
+++ src/WordGram.java
@@ -1,33 +1,42 @@
 /**
  * WordGram objects represent a k-gram of strings/words.
- * 
- * @author YOUR-NAME
+ *
+ * @author Siyun Lee
  *
  */
 
+import java.util.*;
 public class WordGram {
-	
-	private String[] myWords;   
+
+	private String[] myWords;
 	private String myToString;  // cached string
 	private int myHash;         // cached hash value
 
 	/**
-	 * Create WordGram (add comments)
+	 * Creates a new WordGram from the given parameters
 	 * @param source
 	 * @param start
 	 * @param size
 	 */
 	public WordGram(String[] source, int start, int size) {
+
 		myWords = new String[size];
-		// TODO: initialize myWords and ...
+
+		for (int i = 0; i < size; i += 1) {
+			myWords[i] = source[start];
+			start += 1;
+		}
+		myToString = null;
+		myHash = 0;
 	}
 
 	/**
 	 * Return string at specific index in this WordGram
-	 * @param index in range [0..length() ) for string 
+	 * @param index in range [0..length() ) for string
 	 * @return string at index
 	 */
 	public String wordAt(int index) {
+
 		if (index < 0 || index >= myWords.length) {
 			throw new IndexOutOfBoundsException("bad index in wordAt "+index);
 		}
@@ -35,46 +44,61 @@ public class WordGram {
 	}
 
 	/**
-	 * Complete this comment
-	 * @return
+	 * Return the number of words in this WordGram
+	 * @return the number of strings that make up the WordGram
 	 */
 	public int length(){
-		// TODO: change this
-		return 0;
+
+		return myWords.length;
 	}
 
 
 	@Override
 	public boolean equals(Object o) {
+
 		if (! (o instanceof WordGram) || o == null){
 			return false;
 		}
-
-	    // TODO: complete this method
-		return true;
+		WordGram wg = (WordGram) o;
+		return Arrays.equals(this.myWords, wg.myWords);
 	}
 
 	@Override
 	public int hashCode(){
-		// TODO: complete this method
+
+		if (myHash == 0) {
+			myHash = this.toString().hashCode();
+		}
 		return myHash;
 	}
-	
+
 
 	/**
-	 * Create and complete this comment
+	 * Creates a new WordGram using this WordGram as a frame. It places the String
+	 * last at the last index, removes the String at the first index, and shifts
+	 * everything over.
 	 * @param last is last String of returned WordGram
-	 * @return
+	 * @return a new WordGram with last as the last string and all other strings
+	 * shifted over one index
 	 */
 	public WordGram shiftAdd(String last) {
-		WordGram wg = new WordGram(myWords,0,myWords.length);
-		// TODO: Complete this method
+
+		String[] newMyWords = new String[myWords.length];
+
+		for (int i = 0; i < myWords.length - 1; i += 1) {
+			newMyWords[i] = myWords[i+1];
+		}
+		newMyWords[newMyWords.length - 1] = last;
+		WordGram wg = new WordGram(newMyWords,0,newMyWords.length);
 		return wg;
 	}
 
 	@Override
 	public String toString(){
-		// TODO: Complete this method	
+
+		if (myToString == null) {
+			myToString = String.join(" ", myWords);
+		}
 		return myToString;
 	}
 }
--- src/WordGramDriver.java
+++ src/WordGramDriver.java
@@ -2,37 +2,40 @@ import java.io.*;
 import java.util.*;
 
 public class WordGramDriver {
-	public final static int WSIZE = 3;
+	public final static int WSIZE = 6;
 
+// BENCHMARK
 	public static int benchmark(Set<WordGram> set, String filename) throws FileNotFoundException {
 		Scanner scan = new Scanner(new File(filename));
 
-		ArrayList<String> list = new ArrayList<>();
+		ArrayList<String> list = new ArrayList<>(); //ArrayList created with all words in file
 		while (scan.hasNext()) {
 			String s = scan.next();
 			list.add(s);
 		}
 		scan.close();
-		String[] words = list.toArray(new String[0]);
+		String[] words = list.toArray(new String[0]); //Array created from ArrayList
 		for(int k=0; k < words.length - WSIZE + 1; k += 1) {
 			WordGram wg = new WordGram(words,k,WSIZE);
 			set.add(wg);
 		}
 		return words.length - WSIZE + 1;
 	}
+
+	// BENCHMARKSHIFT
 	public static int benchmarkShift(Set<WordGram> set, String filename) throws FileNotFoundException {
 		Scanner scan = new Scanner(new File(filename));
 
-		String[] words = new String[WSIZE];
+		String[] words = new String[WSIZE]; //Array of size WSIZE created
 		for(int k=0; k < WSIZE; k += 1) {
 			words[k] = scan.next();
 		}
 		int count = WSIZE;
 		WordGram current = new WordGram(words,0,WSIZE);
 		set.add(current);
-		
+
 		while (scan.hasNext()) {
-			String s = scan.next();		
+			String s = scan.next();
 			current = current.shiftAdd(s);
 			set.add(current);
 			count += 1;
@@ -40,7 +43,7 @@ public class WordGramDriver {
 		scan.close();
 		return count - WSIZE + 1;
 	}
-	
+
 	public static void main(String[] args) throws FileNotFoundException {
 		String[] plays = {"allswell.txt",
 				"caesar.txt",	"errors.txt",
@@ -53,11 +56,11 @@ public class WordGramDriver {
 		double start,end,time;
 
 		// calculate stats for running benchmark
-		
+
 		int total = 0;
 		start = System.nanoTime();
 		for(String play : plays) {
-			String fname = "data/shakes/" + play;
+			String fname = "../data/shakes/" + play;
 			total += benchmark(hset,fname);
 		}
 		end = System.nanoTime();
@@ -65,13 +68,15 @@ public class WordGramDriver {
 
 		System.out.printf("benchmark time: %1.3f, size = %d\n",time,hset.size());
 		System.out.printf("total # wgs = %d\n",total);
-		
+		//CALCULATE THE DIFFERENCE :)
+		System.out.println(total-hset.size());
+
 		// calculate stats for running benchmarkShift
-		
+
 		total = 0;
 		start = System.nanoTime();
 		for(String play : plays) {
-			String fname = "data/shakes/" + play;
+			String fname = "../data/shakes/" + play;
 			total += benchmarkShift(hset2,fname);
 		}
 		end = System.nanoTime();
@@ -79,13 +84,15 @@ public class WordGramDriver {
 
 		System.out.printf("\nbenchmarkShift time: %1.3f, size = %d\n",time,hset2.size());
 		System.out.printf("total # wgs = %d\n",total);
-		
+		//CALCULATE THE DIFFERENCE :)
+		System.out.println(total-hset2.size());
+
 		HashSet<WordGram> copy = new HashSet<>(hset);
 		HashSet<WordGram> copy2 = new HashSet<>(hset2);
-		
+
 		copy.removeAll(hset2);
 		copy2.removeAll(hset);
-		
+
 		System.out.printf("\nsize of set difference %d\n", copy.size());
 		System.out.printf("size of set2 difference %d\n", copy2.size());
 	}
